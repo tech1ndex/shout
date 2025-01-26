433b5b9a52b3666def515234f3418f5627ed5d70
Victor Bajada <bajada.victor@gmail.com>
2025-01-26 14:25:07 -0500

    Add Logger

--- src/shout/main.py
+++ src/shout/main.py
@@ -5,12 +5,13 @@ from datetime import datetime
 import requests
 import vlc
 import zoneinfo
+from logger import logger
 from src.shout.settings import Settings
 
 settings = Settings(
-    api_url="https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?",
-    team_name="Bills",
-    victory_sound_path="/Users/tech1ndex/Desktop/GoBills.m4a",
+    api_url="https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?", # If using ESPN API
+    team_name="Bills", # Team name to track
+    victory_sound_path="/some/path/GoBills.m4a", # Path to sound file
     )
 
 current_date = datetime.now(zoneinfo.ZoneInfo('America/New_York')).strftime("%Y%m%d")
@@ -27,6 +28,7 @@ def get_team_score(data: json, team_name: str) -> dict | None:
             for competitor in competitors:
                 if team_name.lower() in competitor['team']['name'].lower():
                     return {
+                        'team': competitor['team']['name'],
                         'score': competitor['score'],
                     }
     return None
@@ -34,16 +36,18 @@ def get_team_score(data: json, team_name: str) -> dict | None:
 
 def main() -> None:
     current_score = int(get_team_score(data=update_score(), team_name=settings.team_name)['score'])
-    print(f"Current score is: {current_score}")
+    current_team = get_team_score(data=update_score(), team_name=settings.team_name)['team']
+    logger.info(f"Team is: {current_team}")
+    logger.info(f"Current score is: {current_score}")
     while True:
         live_score = int(get_team_score(data=update_score(), team_name=settings.team_name)['score'])
         if abs(live_score - current_score) > 1:
-            print(f"Score has been updated to {live_score}")
+            logger.info(f"Score has been updated to {live_score}")
             player = vlc.MediaPlayer(f'file://{settings.victory_sound_path}')
             player.play()
             current_score = live_score
 
-            time.sleep(30) # Frequency to check the score
+            time.sleep(20) # Frequency to check the score
 
 if __name__ == "__main__":
     main()
